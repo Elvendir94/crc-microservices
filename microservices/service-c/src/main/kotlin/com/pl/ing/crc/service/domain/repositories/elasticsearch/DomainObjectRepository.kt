620a37df70f8ef61b60408330c98c8cc83ad5961
kuba-bogacki <jb.bogacki@gmail.com>
2025-05-29 21:49:54 +0200

    New: check commit

---
+++ microservices/service-c/src/main/kotlin/com/pl/ing/crc/service/domain/repositories/elasticsearch/DomainObjectRepository.kt
@@ -0,0 +1,7 @@
+package com.pl.ing.crc.service.domain.repositories.elasticsearch
+
+import com.pl.ing.crc.service.domain.model.elasticsearch.DomainObject
+import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository
+import java.util.*
+
+interface DomainObjectRepository : ReactiveElasticsearchRepository<DomainObject, UUID>
